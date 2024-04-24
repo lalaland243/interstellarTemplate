8b54197ed68508def5e506418825958bfa2fd8bb
dumb12344 <167947276+dumb12344@users.noreply.github.com>
2024-04-24 07:05:31 -0700

    Push from vsc

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('node:http')
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static("."))
+app.get('/', (req, res) => {
+  res.sendFile("../sizgjkb.html")
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
